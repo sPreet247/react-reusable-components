abb1fa34cc0c0b1f5c160018ac4fa959cfe90f9e
spreet2472 <samre2472@gmail.com>
2021-02-12 11:25:00 -0500

    initialCommit

---
+++ src/components/Button/ButtonElements.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const ButtonDefault = styled.button`
+  position: absolute;
+  width: 81px;
+  height: 36px;
+  left: 319px;
+  top: 141px;
+
+  background-color: #e0e0e0;
+  box-shadow: 0px 2px 3px rgba(51, 51, 51, 0.2);
+  border-radius: 6px;
+`;
