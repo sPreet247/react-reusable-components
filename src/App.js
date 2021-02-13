50712efc8656dc4682df52540b312290d0ac537e
spreet2472 <samre2472@gmail.com>
2021-02-13 00:47:50 -0500

    defaultBtn

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import './App.css';
-// import Button from './components/Button/Button.js';
 import { ButtonDefault } from './components/Button/ButtonElements';
 
 function App() {
   return (
-    <div className='Default-light'>
+    <>
       <ButtonDefault>Default</ButtonDefault>
-    </div>
+    </>
   );
 }
 
