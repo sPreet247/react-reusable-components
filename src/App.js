1ac420dafbdeea042f75c36dcaa16d2d8a3abebb
spreet2472 <samre2472@gmail.com>
2021-02-23 10:13:49 -0500

    add

--- src/App.js
+++ src/App.js
@@ -19,33 +19,85 @@ function App() {
         <code>&:hover, &:focus</code>
       </HeadingText>
       <BtnDefault default2>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault outline>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault outline2>Default</BtnDefault>
-      <BtnDefault type='text' text>
-        Default
-      </BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
+      <BtnDefault text>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault text2>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault disabledShadow>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault disabled>Disabled</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault disabledText>Disabled</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault groceryStore>
         <AddShoppingCartOutlinedIcon />
         Default
       </BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault groceryStoreRight>
         <AddShoppingCartOutlinedIcon />
         Default
       </BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault smDefault>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault mdDefault>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault lgDefault>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault default1New>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault blueDefault>Default</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault secondaryDefault>Secondary</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault dangerDefault>Danger</BtnDefault>
+      <HeadingText default2Desc>
+        <code>&:hover, &:focus</code>
+      </HeadingText>
       <BtnDefault default2New>Default</BtnDefault>
+
       <BtnDefault darkBlueDefault>Default</BtnDefault>
+
       <BtnDefault secondaryDarkDefault>Secondary</BtnDefault>
+
       <BtnDefault dangerDarkDefault>Danger</BtnDefault>
       <IconLink>
         Icons: https://material.io/resources/icons/?style=round
