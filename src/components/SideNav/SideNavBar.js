5bbbd0d600e719852aca7eb5b09e4c6754af1c94
spreet2472 <samre2472@gmail.com>
2021-02-26 23:17:55 -0500

    addedNavLinks

--- src/components/SideNav/SideNavBar.js
+++ src/components/SideNav/SideNavBar.js
@@ -1,9 +1,15 @@
-import { Title } from './SideNavBarElements';
+import { NavLink, Title } from './SideNavBarElements';
 function SideNavBar() {
   return (
     <>
       <nav>
         <Title>Devchallenges.io</Title>
+        <NavLink colors>Colors</NavLink>
+        <NavLink typography>Typography</NavLink>
+        <NavLink spaces>Spaces</NavLink>
+        <NavLink buttons>Buttons</NavLink>
+        <NavLink inputs>Inputs</NavLink>
+        <NavLink grid>Grid</NavLink>
       </nav>
     </>
   );
--- src/components/SideNav/SideNavBarElements.js
+++ src/components/SideNav/SideNavBarElements.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Title = styled.h3`
   position: absolute;
@@ -16,4 +16,58 @@ export const Title = styled.h3`
   /* identical to box height */
 
   color: #f7542e;
+`;
+
+export const NavLink = styled.a`
+  position: absolute;
+
+  height: 20px;
+  left: 48px;
+
+  font-family: Noto Sans JP;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+
+  color: #9e9e9e;
+
+  ${(props) =>
+    props.colors &&
+    css`
+      top: 180px;
+      width: 62px;
+    `}
+  ${(props) =>
+    props.typography &&
+    css`
+      top: 225px;
+      width: 80px;
+    `}
+  ${(props) =>
+    props.spaces &&
+    css`
+      top: 277px;
+      width: 47px;
+    `}
+  ${(props) =>
+    props.buttons &&
+    css`
+      top: 327px;
+      width: 53px;
+      font-weight: bold;
+      color: #090f31;
+    `}
+  ${(props) =>
+    props.inputs &&
+    css`
+      top: 375px;
+      width: 45px;
+    `}
+  ${(props) =>
+    props.grid &&
+    css`
+      top: 430px;
+      width: 29px;
+    `}
 `;
